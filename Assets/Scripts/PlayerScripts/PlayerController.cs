3503b345c0d233e4de848d975e3e356d8610ef89
OlleReberg <ollereberg@hotmail.com>
2023-04-26 15:38:06 +0200

    Physics and GRavity

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -2,21 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement & Rotation")]
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private float rotationSpeed = 500f;
+    
+    [Header("Ground Check Settings")]
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Vector3 groundCheckOffset;
+    [SerializeField] private LayerMask groundLayer;
 
+    private bool _isGrounded;
+    private float ySpeed;
+    
     private Quaternion targetRotation;
     private Animator animator;
     private CameraController cameraController;
+    private CharacterController characterController;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
         animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -27,12 +39,25 @@ public class PlayerController : MonoBehaviour
         float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
         
         var moveInput = new Vector3(h, 0, v).normalized;
-
         var moveDir = cameraController.PlanarRotation * moveInput;
 
+        var velocity = moveDir * moveSpeed;
+
+        GroundCheck();
+        if (_isGrounded)
+        {
+            ySpeed = -0.5f;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+        velocity.y = ySpeed;
+        
+        characterController.Move(velocity * Time.deltaTime);
+        
         if (moveAmount > 0)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
@@ -41,5 +66,17 @@ public class PlayerController : MonoBehaviour
         
         animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
 
+    }
+
+    void GroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset),
+            groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
