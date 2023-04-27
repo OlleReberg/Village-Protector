017f3fd3c4e387ed0a742e6cfbb899e82174d66c
OlleReberg <ollereberg@hotmail.com>
2023-04-27 15:40:05 +0200

    Added movement behaviour using the A* classes. Yet to be tested

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private AStarPathfinder pathfinder;
+    private EnemyStats enemyStats;
+    
+    void Awake()
     {
-        
+        pathfinder = GetComponent<AStarPathfinder>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void MoveToTarget(Vector3 targetPosition)
     {
-        
+        List<Node> path = pathfinder.FindPath(transform.position, targetPosition);
+
+        // Start at the second node in the path (the first node is the current position)
+        int currentNodeIndex = 1;
+
+        // Move to the first node in the path
+        Vector3 currentTarget = path[currentNodeIndex].position;
+        StartCoroutine(MoveToNode(currentTarget));
+
+        IEnumerator MoveToNode(Vector3 target)
+        {
+            while (transform.position != target)
+            {
+                // Move towards the target node
+                transform.position = Vector3.MoveTowards(transform.position, target, enemyStats.MovementSpeed * Time.deltaTime);
+
+                // If we've reached the current target node, update the target to the next node in the path
+                if (transform.position == target && currentNodeIndex < path.Count - 1)
+                {
+                    currentNodeIndex++;
+                    currentTarget = path[currentNodeIndex].position;
+                }
+
+                yield return null;
+            }
+        }
     }
 }
