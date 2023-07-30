b9628de7ed5ad844817ab1f5091c36456e51182f
OlleReberg <ollereberg@hotmail.com>
2023-07-30 11:57:49 +0200

    Comments added to menutoggler

--- Assets/Scripts/UI Scripts/MenuToggler.cs
+++ Assets/Scripts/UI Scripts/MenuToggler.cs
@@ -5,52 +5,77 @@ using UnityEngine;
 
 public class MenuToggler : MonoBehaviour
 {
-   private bool isPaused;
-   [SerializeField] private GameObject inventoryPanel;
-   [SerializeField] private CameraController cameraController;
+    private bool isPaused; // Flag to track if the game is paused or not
 
-   private void Awake()
-   {
-      inventoryPanel.SetActive(false);
-      isPaused = false;
-   }
+    [SerializeField] private GameObject inventoryPanel; // Reference to the inventory panel GameObject
+    [SerializeField] private CameraController cameraController; // Reference to the CameraController script
 
-   private void Update()
-   {
-      ToggleInventory();
-   }
+    private void Awake()
+    {
+        // Deactivate the inventory panel at the start
+        inventoryPanel.SetActive(false);
 
-   public void PauseGame()
-   {
-      Time.timeScale = 0f;
-      isPaused = true;
-      Debug.Log("Game should be paused");
-      cameraController.enabled = false;
+        // Set isPaused to false at the start
+        isPaused = false;
+    }
 
-   }
+    private void Update()
+    {
+        // Check for the input to toggle the inventory
+        ToggleInventory();
+    }
 
-   public void ResumeGame()
-   {
-      Time.timeScale = 1f;
-      isPaused = false;
-      Debug.Log("Game should be unpaused");
-      cameraController.enabled = true;
-   }
+    // Method to pause the game
+    public void PauseGame()
+    {
+        // Set the time scale to 0 to pause the game
+        Time.timeScale = 0f;
 
-   public void ToggleInventory()
-   {
-      if (Input.GetKeyDown(KeyCode.I))
-      {
-         if (!isPaused)
-         {
-            PauseGame();
-            inventoryPanel.SetActive(true);
-         }
-         else if (isPaused)
-         {
-            ResumeGame();
-            inventoryPanel.SetActive(false);
-         }
-      }
-   }
+        // Set isPaused to true to indicate that the game is paused
+        isPaused = true;
+
+        // Disable the CameraController to prevent player movement during the pause
+        cameraController.enabled = false;
+
+        // Log a message to indicate that the game is paused
+        Debug.Log("Game should be paused");
+    }
+
+    // Method to resume the game
+    public void ResumeGame()
+    {
+        // Set the time scale to 1 to resume the game
+        Time.timeScale = 1f;
+
+        // Set isPaused to false to indicate that the game is not paused anymore
+        isPaused = false;
+
+        // Enable the CameraController to allow player movement after resuming
+        cameraController.enabled = true;
+
+        // Log a message to indicate that the game is unpaused
+        Debug.Log("Game should be unpaused");
+    }
+
+    // Method to toggle the inventory panel and pause/resume the game accordingly
+    public void ToggleInventory()
+    {
+        // Check if the 'I' key is pressed
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            // If the game is not paused, pause it and show the inventory panel
+            if (!isPaused)
+            {
+                PauseGame();
+                inventoryPanel.SetActive(true);
+            }
+            // If the game is already paused, resume it and hide the inventory panel
+            else if (isPaused)
+            {
+                ResumeGame();
+                inventoryPanel.SetActive(false);
+            }
+        }
+    }
 }
+
